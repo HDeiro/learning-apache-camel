8d2a74480a36531abb2fcf78d55feb91b1af6c13
Hugo Deiró <hugodeiro@gmail.com>
2018-05-01 21:27:41 -0300

    Adding file copying on a camel route

---
+++ src/main/java/com/learncamel/file/CopyFilesWithoutCamel.java
@@ -0,0 +1,35 @@
+package com.learncamel.file;
+
+import java.io.*;
+
+public class CopyFilesWithoutCamel {
+    public static void main(String[] args) throws IOException {
+        //Step 1: Create a file Object to read the directories
+        //Step 2: Read the files from the directory and iterate them
+        //Step 3: Create the output stream to read the files
+        //Step 4: Close the streams
+
+        File inputDirectory = new File("data/input");
+        File outputDirectory = new File("data/output");
+
+        File[] files = inputDirectory.listFiles();
+
+        for(File source : files) {
+            if(source.isDirectory()) return;
+
+            File dest = new File(outputDirectory.getPath() + File.separator + source.getName());
+
+            OutputStream oStream = new FileOutputStream(dest);
+            byte[] buffer = new byte[(int) source.length()];
+            FileInputStream iStream = new FileInputStream(source);
+            iStream.read(buffer);
+
+            try {
+                oStream.write(buffer);
+            } finally {
+                oStream.close();
+                iStream.close();
+            }
+        }
+    }
+}
